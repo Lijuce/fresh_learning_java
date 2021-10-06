b5de8f2997a9c99789b712a0d7e45977824293b4
Lijuce <441214473@qq.com>
2021-10-06 20:28:18 +0800

    自定义消息协议考虑黏包拆包问题

--- Net网络编程/NettyDemo/ProtoDesign/src/main/java/TestCustomProtoMessageCodec.java
+++ Net网络编程/NettyDemo/ProtoDesign/src/main/java/TestCustomProtoMessageCodec.java
@@ -1,6 +1,7 @@
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import message.LoginRequestMessage;
@@ -18,6 +19,8 @@ public class TestCustomProtoMessageCodec {
     public static void main(String[] args) throws Exception {
         EmbeddedChannel channel = new EmbeddedChannel(
                 new LoggingHandler(LogLevel.INFO),
+                // 此处添加黏拆包解码器
+                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
                 new MessageCodec());
 
         // encode编码测试
@@ -28,7 +31,19 @@ public class TestCustomProtoMessageCodec {
         // decode解码测试
         ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
         new MessageCodec().encode(null, loginRequestMessage, buf);
-        // 入站
-        channel.writeInbound(buf);
+
+        // 测试黏包拆包问题
+        // 先通过slice方法人为制造拆包现象
+        ByteBuf s1 = buf.slice(0, 100);
+        ByteBuf s2 = buf.slice(100, buf.readableBytes()-100);
+        // 引用计数 +1
+        s1.retain();
+
+//        // 入站
+//        channel.writeInbound(buf);
+        // 由于入栈会进行引用计数 -1 操作，因此上面需要进行 +1 操作
+        channel.writeInbound(s1);
+        channel.writeInbound(s2);
+
     }
 }
